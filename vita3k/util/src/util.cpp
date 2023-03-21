b98b69e2f7fd05ac5bc64a8b9d96f3a9c179b9fb
GamingDuck <90236481+GamingDucking@users.noreply.github.com>
2023-03-21 14:34:54 +0200

    Regress Bookmist's Logging code
    Reported being not entering to any game according to Intel Graphics users.

--- vita3k/util/src/util.cpp
+++ vita3k/util/src/util.cpp
@@ -26,15 +26,6 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 #ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#else
-#include <signal.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#endif
-
-#ifdef WIN32
 #include <winsock2.h>
 #else
 #include <fcntl.h>
@@ -60,8 +51,6 @@ static const fs::path &LOG_FILE_NAME = "vita3k.log";
 static const char *LOG_PATTERN = "%^[%H:%M:%S.%e] |%L| [%!]: %v%$";
 std::vector<spdlog::sink_ptr> sinks;
 
-void register_log_exception_handler();
-
 void flush() {
     spdlog::details::registry::instance().flush_all();
 }
@@ -78,11 +67,6 @@ ExitCode init(const Root &root_paths, bool use_stdout) {
         std::cerr << "spdlog error: " << msg << std::endl;
         assert(0);
     });
-
-#ifdef WIN32
-    // set console codepage to UTF-8
-    SetConsoleOutputCP(65001);
-#endif
 
     register_log_exception_handler();
 
@@ -147,68 +131,6 @@ int ret_error_impl(const char *name, const char *error_str, std::uint32_t error_
 
     return error_val;
 }
-
-// log exceptions and flush log file on exceptions
-#ifdef WIN32
-static LONG WINAPI exception_handler(PEXCEPTION_POINTERS pExp) noexcept {
-    const unsigned ec = pExp->ExceptionRecord->ExceptionCode;
-    switch (ec) {
-    case EXCEPTION_ACCESS_VIOLATION:
-        LOG_CRITICAL("Exception EXCEPTION_ACCESS_VIOLATION ({}). ", log_hex(ec));
-        switch (pExp->ExceptionRecord->ExceptionInformation[0]) {
-        case 0:
-            LOG_CRITICAL("Read violation at address {}.", log_hex(pExp->ExceptionRecord->ExceptionInformation[1]));
-            break;
-        case 1:
-            LOG_CRITICAL("Write violation at address {}.", log_hex(pExp->ExceptionRecord->ExceptionInformation[1]));
-            break;
-        case 8:
-            LOG_CRITICAL("DEP violation at address {}.", log_hex(pExp->ExceptionRecord->ExceptionInformation[1]));
-            break;
-        default:
-            break;
-        }
-        break;
-    case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
-        LOG_CRITICAL("Exception EXCEPTION_ARRAY_BOUNDS_EXCEEDED ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_DATATYPE_MISALIGNMENT:
-        LOG_CRITICAL("Exception EXCEPTION_DATATYPE_MISALIGNMENT ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_FLT_DIVIDE_BY_ZERO:
-        LOG_CRITICAL("Exception EXCEPTION_FLT_DIVIDE_BY_ZERO ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_ILLEGAL_INSTRUCTION:
-        LOG_CRITICAL("Exception EXCEPTION_ILLEGAL_INSTRUCTION ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_IN_PAGE_ERROR:
-        LOG_CRITICAL("Exception EXCEPTION_IN_PAGE_ERROR ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_INT_DIVIDE_BY_ZERO:
-        LOG_CRITICAL("Exception EXCEPTION_INT_DIVIDE_BY_ZERO ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_PRIV_INSTRUCTION:
-        LOG_CRITICAL("Exception EXCEPTION_PRIV_INSTRUCTION ({}). ", log_hex(ec));
-        break;
-    case EXCEPTION_STACK_OVERFLOW:
-        LOG_CRITICAL("Exception EXCEPTION_STACK_OVERFLOW ({}). ", log_hex(ec));
-        break;
-    default:
-        return EXCEPTION_CONTINUE_SEARCH;
-    }
-    flush();
-    return EXCEPTION_CONTINUE_SEARCH;
-}
-
-void register_log_exception_handler() {
-    if (!AddVectoredExceptionHandler(0, exception_handler)) {
-        LOG_CRITICAL("Failed to register an exception handler");
-    }
-}
-
-#else
-void register_log_exception_handler() {}
-#endif
 } // namespace logging
 
 namespace string_utils {
